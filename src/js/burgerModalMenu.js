d3777a9a91dbe78addc2e88f50bfd573ec83623d
Alyonamk <110933584+Alyonamk@users.noreply.github.com>
2023-11-12 23:46:52 +0200

    //updated

--- src/js/burgerModalMenu.js
+++ src/js/burgerModalMenu.js
@@ -1,6 +1,6 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const burgerOpenBtn = document.querySelector('.burger-modal-open');
-// const mobileClose = document.querySelector('.mobile-open-modal');
+const mobileClose = document.querySelector('.mobile-open-modal');
 const burgerCloseBtn = document.querySelector('.burger-modal-close-btn');
 
 const homeLink = document.getElementById('home');
@@ -23,8 +23,8 @@ const closeBurgerMenu = () => {
 // Add event listeners to open and close the burger menu
 burgerOpenBtn.addEventListener('click', openBurgerMenu);
 burgerCloseBtn.addEventListener('click', closeBurgerMenu);
-// mobileClose.addEventListener('click', closeBurgerMenu);
+mobileClose.addEventListener('click', closeBurgerMenu);
 
-// homeLink.addEventListener('click', closeBurgerMenu);
-// howMadeLink.addEventListener('click', closeBurgerMenu);
-// contactsLink.addEventListener('click', closeBurgerMenu);
+homeLink.addEventListener('click', closeBurgerMenu);
+howMadeLink.addEventListener('click', closeBurgerMenu);
+contactsLink.addEventListener('click', closeBurgerMenu);
