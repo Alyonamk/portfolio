b6c288ea6f1fe8c4035f1e66fdaa70e41dc3781f
Alyonamk <110933584+Alyonamk@users.noreply.github.com>
2023-12-05 22:38:46 +0200

    //

--- src/js/burgerModalMenu.js
+++ src/js/burgerModalMenu.js
@@ -1,10 +1,40 @@
+// const burgerMenu = document.querySelector('.burger-menu');
+// const burgerOpenBtn = document.querySelector('.burger-modal-open');
+// const burgerCloseBtn = document.querySelector('.burger-modal-close-btn');
+
+// const homeLink = document.getElementById('home');
+// const aboutLink = document.getElementById('about-me');
+// const contactsLink = document.getElementById('contacts');
+// const productsLink = document.getElementById('products');
+
+// // Function to open burger menu
+// const openBurgerMenu = () => {
+//   burgerMenu.classList.remove('is-hidden');
+//   burgerMenu.classList.add('open');
+// };
+
+// // Function to close the burger menu
+// const closeBurgerMenu = () => {
+//   burgerMenu.classList.add('is-hidden');
+//   burgerMenu.classList.remove('open');
+// };
+
+// // Add event listeners to open and close the burger menu
+// burgerOpenBtn.addEventListener('click', openBurgerMenu);
+// burgerCloseBtn.addEventListener('click', closeBurgerMenu);
+
+
+// homeLink.addEventListener('click', closeBurgerMenu);
+// aboutLink.addEventListener('click', closeBurgerMenu);
+// contactsLink.addEventListener('click', closeBurgerMenu);
+
+
 const burgerMenu = document.querySelector('.burger-menu');
 const burgerOpenBtn = document.querySelector('.burger-modal-open');
-const mobileClose = document.querySelector('.mobile-open-modal');
 const burgerCloseBtn = document.querySelector('.burger-modal-close-btn');
 
 const homeLink = document.getElementById('home');
-const howMadeLink = document.getElementById('how-made');
+const howMadeLink = document.getElementById('about');
 const contactsLink = document.getElementById('contacts');
 const productsLink = document.getElementById('products');
 
@@ -28,3 +58,4 @@ mobileClose.addEventListener('click', closeBurgerMenu);
 homeLink.addEventListener('click', closeBurgerMenu);
 howMadeLink.addEventListener('click', closeBurgerMenu);
 contactsLink.addEventListener('click', closeBurgerMenu);
+productsLink.addEventListener('click', closeBurgerMenu);
