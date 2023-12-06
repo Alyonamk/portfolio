22ed90ac6ba5eb6c4582a27e42f4e86d5b7a660a
Alyonamk <110933584+Alyonamk@users.noreply.github.com>
2023-12-06 18:25:34 +0200

    /

--- src/js/burgerModalMenu.js
+++ src/js/burgerModalMenu.js
@@ -1,61 +1,22 @@
-// const burgerMenu = document.querySelector('.burger-menu');
-// const burgerOpenBtn = document.querySelector('.burger-modal-open');
-// const burgerCloseBtn = document.querySelector('.burger-modal-close-btn');
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.burger-modal-open'),
+    closeMenuBtn: document.querySelector('.burger-modal-close-btn'),
+    menu: document.querySelector('.burger-menu'),
+    menuList: document.querySelector('.burger-list'),
+  };
+  
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
 
-// const homeLink = document.getElementById('home');
-// const aboutLink = document.getElementById('about-me');
-// const contactsLink = document.getElementById('contacts');
-// const productsLink = document.getElementById('products');
-
-// // Function to open burger menu
-// const openBurgerMenu = () => {
-//   burgerMenu.classList.remove('is-hidden');
-//   burgerMenu.classList.add('open');
-// };
-
-// // Function to close the burger menu
-// const closeBurgerMenu = () => {
-//   burgerMenu.classList.add('is-hidden');
-//   burgerMenu.classList.remove('open');
-// };
-
-// // Add event listeners to open and close the burger menu
-// burgerOpenBtn.addEventListener('click', openBurgerMenu);
-// burgerCloseBtn.addEventListener('click', closeBurgerMenu);
-
-
-// homeLink.addEventListener('click', closeBurgerMenu);
-// aboutLink.addEventListener('click', closeBurgerMenu);
-// contactsLink.addEventListener('click', closeBurgerMenu);
-
-
-const burgerMenu = document.querySelector('.burger-menu');
-const burgerOpenBtn = document.querySelector('.burger-modal-open');
-const burgerCloseBtn = document.querySelector('.burger-modal-close-btn');
-
-const homeLink = document.getElementById('home');
-const howMadeLink = document.getElementById('about');
-const contactsLink = document.getElementById('contacts');
-const productsLink = document.getElementById('products');
-
-// Function to open burger menu
-const openBurgerMenu = () => {
-  burgerMenu.classList.remove('is-hidden');
-  burgerMenu.classList.add('open');
-};
-
-// Function to close the burger menu
-const closeBurgerMenu = () => {
-  burgerMenu.classList.add('is-hidden');
-  burgerMenu.classList.remove('open');
-};
-
-// Add event listeners to open and close the burger menu
-burgerOpenBtn.addEventListener('click', openBurgerMenu);
-burgerCloseBtn.addEventListener('click', closeBurgerMenu);
-mobileClose.addEventListener('click', closeBurgerMenu);
-
-homeLink.addEventListener('click', closeBurgerMenu);
-howMadeLink.addEventListener('click', closeBurgerMenu);
-contactsLink.addEventListener('click', closeBurgerMenu);
-productsLink.addEventListener('click', closeBurgerMenu);
+  function toggleMenu() {
+    document.body.classList.toggle('no-scroll');
+    refs.menu.classList.toggle('is-hidden');
+  }
+  function removeMenu() {
+    refs.menu.classList.add('is-hidden');
+    document.body.classList.remove('no-scroll');
+  }
+  
+})();
