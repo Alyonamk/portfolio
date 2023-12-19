dc3ba7a18f519cae3091f78205b8f25858b9cfaa
Alyonamk <110933584+Alyonamk@users.noreply.github.com>
2023-12-19 12:23:15 +0000

    Deploying to gh-pages from @ Alyonamk/portfolio@5301c0de8c0c7ec27d71ed1060b06123fd9f6d7d 🚀

---
+++ index.b548b420.js
@@ -0,0 +1,2 @@
+!function(){var e=new IntersectionObserver((function(e){e.forEach((function(e){console.log(e),e.isIntersecting?e.target.classList.add("show"):e.target.classList.remove("show")}))}));document.querySelectorAll(".hidden").forEach((function(n){return e.observe(n)}))}();
+//# sourceMappingURL=index.b548b420.js.map
